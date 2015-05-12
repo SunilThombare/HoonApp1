73a4ee26e388f37fc57c51c322554d651c347faf
hoon <dokkl@naver.com>
2015-05-12 22:15:42 +0900

    [h-1]로고추가

--- app/src/main/java/com/babybong/appting/SplashActivity.java
+++ app/src/main/java/com/babybong/appting/SplashActivity.java
@@ -21,6 +21,9 @@ public class SplashActivity extends BaseActivity {
     SharedPreferences setting;
     SharedPreferences.Editor editor;
 
+    // Splash screen timer
+    private static int SPLASH_TIME_OUT = 3000;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -57,7 +60,7 @@ public class SplashActivity extends BaseActivity {
             }
         };
 
-        handler.sendEmptyMessageDelayed(0, 3000);
+        handler.sendEmptyMessageDelayed(0, SPLASH_TIME_OUT);
     }
 
     @Override
